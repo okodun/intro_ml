b9d5c63530cb342574b606b81005f1b0af5dff01
okodun <felix@upndown.de>
2024-10-09 11:28:14 +0300

    Finish exercise 6

--- exercise_6/taxi.py
+++ exercise_6/taxi.py
@@ -4,76 +4,129 @@ import gymnasium as gym
 
 
 def start_manual_game():
-    """Starts a manual game."""
+    """starts a manual game"""
+
     done = False
-    state = env.reset()[0]  # Ensure state is captured from reset
     while not done:
         action = int(input("0/down 1/up 2/right 3/left 4/pick-up 5/drop-off: "))
-        new_state, reward, done, truncated, info = env.step(action)
-        state = new_state
+        _, _, done, _, _ = env.step(action)
         print(env.render())
 
 
-def learn(qtable, lr=0.1, rdecay=0.9, episodes=10000, interactions=100, expl_val=0.01):
-    for _ in range(episodes):
-        state = env.reset()[0]
-        done = False
+class Agent:
+    """class for agent that utilizes Q-Learning"""
 
-        for _ in range(interactions):
-            if np.random.uniform(0, 1) < expl_val:
-                action = np.random.randint(0, qtable.shape[1])
-            else:
-                action = np.argmax(qtable[state, :])
+    def __init__(self, env, lr=0.1, rdecay=0.9, e=10000, i=100, exp=0.01):
+        """creates new Agent object"""
 
-            new_state, reward, done, truncated, info = env.step(action)
+        self.ENVIRONMENT = env
+        self.LEARNING_RATE = lr
+        self.REWARD_DECAY = rdecay
+        self.EPISODES = e
+        self.INTERACTIONS = i
+        self.EXPLORE_VALUE = exp
+        self.QTABLE = None
 
-            # Update the Q-table
-            update = lr * (
-                reward + rdecay * np.max(qtable[new_state, :]) - qtable[state, action]
-            )
-            qtable[state, action] += update
+    def learn(self):
+        """learns optimal behavior in evironment"""
 
+        # initialize Q-Table
+        qtable = np.random.rand(
+            self.ENVIRONMENT.observation_space.n, self.ENVIRONMENT.action_space.n
+        )
+
+        # start iteration over episodes
+        for _ in range(self.EPISODES):
+
+            # define environment and set done flag
+            state = self.ENVIRONMENT.reset()[0]
+            done = False
+
+            # start interacting with environment
+            for _ in range(self.INTERACTIONS):
+
+                # randomly explore environment
+                if np.random.uniform(0, 1) < self.EXPLORE_VALUE:
+                    action = np.random.randint(0, qtable.shape[1])
+                else:
+                    action = np.argmax(qtable[state, :])
+
+                # execute action
+                new_state, reward, done, _, _ = env.step(action)
+
+                # update Q-table value
+                update = self.LEARNING_RATE * (
+                    reward
+                    + self.REWARD_DECAY * np.max(qtable[new_state, :])
+                    - qtable[state, action]
+                )
+                qtable[state, action] += update
+
+                # set new state
+                state = new_state
+
+                # check if solution was found
+                if done:
+                    break
+
+        # save Q-Table
+        self.QTABLE = qtable
+
+    def evaluate(self, max_range=1000):
+        """evaluates the found solution"""
+
+        # initialize result variables
+        total_reward = 0
+        total_steps = 0
+
+        # reset environment
+        state = self.ENVIRONMENT.reset()[0]
+
+        # start playing the game
+        for step in range(max_range):
+
+            # choose action and execute it
+            action = np.argmax(self.QTABLE[state, :])
+            new_state, reward, done, _, _ = env.step(action)
+
+            # update reward and state
+            total_reward += reward
             state = new_state
 
+            # break if done
             if done:
+                total_steps = step
                 break
 
-    return qtable
+        return total_reward, total_steps
 
 
 if __name__ == "__main__":
-    for _ in range(10):
-        r = 0
-        s = 0
-        for j in range(10):
-            # Initialize environment
-            env = gym.make("Taxi-v3", render_mode="ansi")
-            env.reset()
 
-            # Get size of state and action space
-            action_size = env.action_space.n
-            state_size = env.observation_space.n
+    # define number of iterations
+    N = 10
 
-            # Start learning
-            qtable = learn(np.random.rand(state_size, action_size))
+    # initialize result variables
+    total_reward = 0
+    total_num_of_actions = 0
 
-            # Let machine play the game
-            done = False
-            total_reward = 0
-            steps = 0
-            state = env.reset()[0]
-            # print(env.render())
-            for i in range(1000):
-                action = np.argmax(qtable[state, :])
-                new_state, reward, done, truncated, info = env.step(action)
-                total_reward += reward
-                state = new_state
-                # time.sleep(0.5)
-                # print(env.render())
-                if done:
-                    steps = i
-                    break
-            # print(f"Total Reward: {total_reward} / Steps: {steps}")
-            r += total_reward
-            s += steps
-        print(f"Total Reward: {r / 10} / Steps: {s / 10}")
+    # start iterations
+    for _ in range(N):
+
+        # initialize environment
+        env = gym.make("Taxi-v3", render_mode="ansi")
+        env.reset()
+
+        # create agent and learn
+        agent = Agent(env)
+        agent.learn()
+
+        # evaluate and save results
+        evaluation = agent.evaluate()
+        total_reward += evaluation[0]
+        total_num_of_actions += evaluation[1]
+
+    # print result
+    print(f"The agent produced an average reward of {total_reward / N}.")
+    print(f"He took {total_num_of_actions / N} steps on average to solve the problem.")
+    print(f"The agent solved {N} different problems.")
